abc159297d84103745d8b450404c332301067d4e
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2021-06-16 01:56:16 +0100

    Make debug panel draggable around

--- src/DebugPanel.tsx
+++ src/DebugPanel.tsx
@@ -1,4 +1,5 @@
 import { SketchPicker } from 'react-color';
+import Draggable from 'react-draggable';
 import { useRecoilState } from 'recoil';
 
 import { wetlandsBackdropColorState, wetlandsBackdropOpacityState } from './recoil/debugging';
@@ -15,51 +16,48 @@ export function DebugPanel() {
     const [wetlandsOpacity, setWetlandsOpacity] = useRecoilState(wetlandsBackdropOpacityState);
 
     return (
-        <div className="absolute top-40 left-2 z-50 outline-black">
-            <h2 className="text-lg">Wetlands backdrop color?</h2>
-            <div className="flex flex-row gap-1 m-1">
-                {wetlandsOptions.map(({ color, opacity }, i) => (
-                    <button
-                        className="bg-gray-500 text-white rounded"
-                        onClick={() => {
-                            setWetlandsColor(color);
-                            setWetlandsOpacity(opacity);
-                        }}
-                    >
-                        Opt. {i + 1}
-                    </button>
-                ))}
+        <Draggable cancel=".draggable-no">
+            <div className="absolute top-40 left-2 z-50 outline-black bg-gray-50 rounded-xl p-3 pt-5">
+                <div className="draggable-no">
+                    <h2 className="text-lg">Wetlands backdrop color?</h2>
+                    <div className="flex flex-row gap-1 m-1">
+                        {wetlandsOptions.map(({ color, opacity }, i) => (
+                            <button
+                                key={i}
+                                className="bg-gray-500 text-white rounded"
+                                onClick={() => {
+                                    setWetlandsColor(color);
+                                    setWetlandsOpacity(opacity);
+                                }}
+                            >
+                                Opt. {i + 1}
+                            </button>
+                        ))}
+                    </div>
+                    <div className="m-2">
+                        <h3>Opacity</h3>
+                        <input
+                            type="range"
+                            id="wetlands-color"
+                            min={0}
+                            max={1}
+                            step={0.05}
+                            value={wetlandsOpacity}
+                            onChange={e => setWetlandsOpacity(parseFloat(e.target.value))}
+                        ></input>{' '}
+                        <div className="inline-block w-5">{wetlandsOpacity.toFixed(2)}</div>
+                    </div>
+                    <div>
+                        <h3>Color</h3>
+                        <SketchPicker
+                            color={wetlandsColor}
+                            onChange={color => setWetlandsColor(color.hex)}
+                            disableAlpha={true}
+                            presetColors={[]}
+                        />
+                    </div>
+                </div>
             </div>
-            <div>
-                <h3>Opacity</h3>
-                <input
-                    type="range"
-                    id="wetlands-color"
-                    min={0}
-                    max={1}
-                    step={0.05}
-                    value={wetlandsOpacity}
-                    onChange={e => setWetlandsOpacity(parseFloat(e.target.value))}
-                ></input>{' '}
-                {wetlandsOpacity}
-            </div>
-            <div>
-                <h3>Color</h3>
-                <SketchPicker
-                    color={wetlandsColor}
-                    onChange={color => setWetlandsColor(color.hex)}
-                    disableAlpha={true}
-                    presetColors={[]}
-                />
-                {/* <input
-                    type="text"
-                    id="wetlands-opacity"
-                    minLength={4}
-                    maxLength={7}
-                    value={wetlandsColor}
-                    onChange={e => setWetlandsColor(e.target.value)}
-                ></input> */}
-            </div>
-        </div>
+        </Draggable>
     );
 }
