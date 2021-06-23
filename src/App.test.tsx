f92551f42cf3da2f92c1157eb1cbcbe4dfaba708
GitHub Action <action-noreply@github.com>
2021-06-23 20:41:33 +0000

    Create build for revision 29142f820ee28ec409d0b84c55b4a9d62b5e3fc2

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable*/
-import React from 'react';
-
-test('renders data selection form', () => {
-    // do nothing
-    //   render(<App />);
-    //   const linkElement = screen.getByText(/Index/i);
-    //   expect(linkElement).toBeInTheDocument();
-});
--- src/config.ts
+++
@@ -1,9 +0,0 @@
-export const config = {
-    initialViewport: {
-        width: 400,
-        height: 400,
-        latitude: 19.37,
-        longitude: -99.05,
-        zoom: 10.5,
-    },
-};
